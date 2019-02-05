6aaf232a36007be4d81c400141f70f5a24f75e0e
Daoudy <31867025+Daoudy@users.noreply.github.com>
2019-02-05 14:35:21 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './models/post.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  posts: Post[] = [];
+
+  constructor() {
+    const p1 = new Post('The First Post', 'A post about Angular....', 3);
+    this.posts.push(p1);
+
+    const p2 = new Post('The Second Post', 'A post about NGRX....', -2);
+    this.posts.push(p2);
+
+    const p3 = new Post('The Thrid Post', 'A post about RXJS....', 0);
+    this.posts.push(p3);
+  }
+
 }
+
